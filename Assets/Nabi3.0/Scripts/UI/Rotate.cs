dd8c8e43d8a12c342889dcfc7863b1465c12cbba
jonama1975 <jonama1975@gmail.com>
2026-04-16 18:30:21 -0500

    CambiosMenuChakras16/04/2026
    Se cambiaron las imágenes del menú de los chakras y los colores de Ed al seleccionar cada botón, por su color correcto

---
+++ Assets/Nabi3.0/Scripts/UI/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public float x;
+    public float y;
+    public float z;
+
+    public bool si;
+    void Update()
+    {
+        if (si)
+        {
+            // Rotate the object around its local X axis at 1 degree per second
+            transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+        }
+        else
+        {
+            // ...also rotate around the World's Y axis
+            transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime, Space.World);
+        }
+
+    }
+}
